13a9dc92b8f683a87476f0d43192beb0b9eabcdb
Ryan Briones <ryanbriones@li-158.members.linode.com>
2009-01-07 21:42:32 +0000

    some production workarounds; figure out later

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -10,4 +10,9 @@ class Tweet
   def text_codemashified
     self.text.gsub(/(#codemash)/i, '<span class="hl_codemash">\1</span>')
   end
+
+  def created_at_codemash_time
+    return self.created_at unless TZ
+    TZ.utc_to_local(self.created_at)
+  end
 end
--- pound_codemash.rb
+++ pound_codemash.rb
@@ -2,12 +2,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'dm-core'
+require 'tzinfo'
 
 require File.expand_path(File.dirname(__FILE__) + '/lib/tweet')
 
 DataMapper.setup(:default, "sqlite3://" + File.expand_path(File.dirname(__FILE__) + '/db/pound_codemash.sqlite3'))
 
-get '/' do
+TZ = TZInfo::Timezone.get('US/Eastern')
+
+get '' do
   @tweets = Tweet.all(:order => [:created_at.desc], :limit => 15)
   erb :index
 end
